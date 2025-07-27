12c3f189bfe909fbbbb3eef70d203a2837dfd882
Kylanhall <git@kylan.net>
2025-07-26 22:46:32 -0400

    Added TestingModel based on database. Added TimeModel based on Web API.

---
+++ Models/TestingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kylan.space.Models
+{
+    public class TestingModel
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
---
+++ Models/TimeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kylan.space.Models
+{
+    public class TimeModel
+    {
+        public int year { get; set; }
+        public int month { get; set; }
+        public int day { get; set; }
+        public int hour { get; set; }
+        public int minute { get; set; }
+        public int second { get; set; }
+        public int milliSeconds { get; set; }
+        public string? dateTime { get; set; }
+        public string? date { get; set; }
+        public string? time { get; set; }
+        public string? timeZone { get; set; }
+        public string? dayOfWeek { get; set; }
+        public bool dstActive { get; set; }
+    }
+}
