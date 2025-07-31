d8538329b89dfe1b08ce3685b1109514962f6227
Kylanhall <git@kylan.net>
2025-07-30 20:47:10 -0400

    ADD: DBLibrary now part of main website codebase, rather than imorted library.

---
+++ DBLibrary/DBAccess.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLibrary
+{
+    public class DBAccess : IDBAccess
+    {
+        // string SQL: Passing in SQL statement. Example: Select * from *table name*
+        // U Parameters: Generic being used, can send *anything* using it. Any data type, class, etc.
+        // string connectionString: Connection string, how we connect to DB
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<T>(sql, parameters);
+
+                return rows.ToList();
+            }
+        }
+
+        public async Task SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(sql, parameters);
+            }
+        }
+    }
+}
