4737774d25ec8a24bf2472bb47347ae4bd05de8d
Kylanhall <git@kylan.net>
2025-08-31 20:13:34 -0400

    Added DataAnnotations, BlazorCaptcha, and some other stuff i forgot

--- Models/TestingModel.cs
+++ Models/TestingModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,14 @@ namespace kylan.space.Models
     public class TestingModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Input is Required.")]
         public string firstName { get; set; }
+        [Required(ErrorMessage = "Input is Required.")]
         public string lastName { get; set; }
+        [Required]
+        public string captcha { get; set; }
+        [Required(ErrorMessage = "Captcha is required.")]
+        [Compare("captcha", ErrorMessage = "Captcha is incorrect.")]
+        public string captchaResponse { get; set; }
     }
 }
